7ebccb5f38d13ff88f21c78c36855946a6ace6eb
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-11-30 11:00:07 -0300

    Implement loops class files

---
+++ loops/index.js
@@ -0,0 +1,24 @@
+// Os 3 principais loops em Js => For, While e Do-While
+
+// For
+// Imprimir os números de 1 a 5
+for (let i = 1; i <= 5; i++) {
+  console.log(`Variável i é ${i}`);
+}
+
+// While
+// Imprimir os números de 1 a 5
+let contador = 1;
+
+while (contador <= 5) {
+  console.log("Contador" + " " + contador);
+  contador++;
+}
+
+// Do-While
+let index = 1;
+
+do {
+console.log(`Current index is ${index}`);
+index++;
+} while (index <= 5);
