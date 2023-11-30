2ba97be2aad6e1fcb867ae747d815a4210abe959
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-11-30 09:41:16 -0300

    first commit

---
+++ variaveis/index.js
@@ -0,0 +1,25 @@
+// Calculando a média
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 9;
+
+const media = (nota1 + nota2 + nota3) / 3;
+console.log(`A média é => ${media}`);
+
+// Calculando o IMC
+let idade = 25;
+let peso = 68.5;
+let altura = 1.75;
+
+const imc = peso / (altura ** 2);
+console.log(`O IMC é de => ${imc}`);
+
+// Declarando variáveis de texto
+let nome = 'Maria';
+let sobrenome = 'Silva';
+
+// Concatenando strings
+let nomeCompleto = nome + " " + sobrenome;
+console.log("Nome completo", nomeCompleto);
+
+
