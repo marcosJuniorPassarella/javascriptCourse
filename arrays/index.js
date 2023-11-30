82a8dbd1c968ade361292874234076f199f9ec55
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-11-30 15:00:43 -0300

    Implement shift() and unshift() examples

--- arrays/index.js
+++ arrays/index.js
@@ -3,24 +3,24 @@ let numeros = [1, 2, 3, 4, 5];
 let frutas = ["Maçã", "Banana", "Morango"];
 
 // Acessando elementos de um Array
-console.log(numeros[2]);
-console.log(frutas[2]);
+// console.log(numeros[2]);
+// console.log(frutas[2]);
 
 // Modificar elementos de um Array
 frutas[0] = "Pêra";
 
 // Adicionando e removendo elementos
 frutas.push("Maça");
-console.log(frutas);
+// console.log(frutas);
 frutas.pop();
-console.log(frutas);
+// console.log(frutas);
 
 // Iterando sobre Arrays
 for (const fruta of frutas) {
-  console.log("Fruta Atual" + " " + fruta);
+  // console.log("Fruta Atual" + " " + fruta);
 }
 
-frutas.forEach((fruta) => console.log(`Fruta atual no forEach ${fruta}`));
+// frutas.forEach((fruta) => console.log(`Fruta atual no forEach ${fruta}`));
 
 // Arrays Multidimensionais
 let matriz = [
@@ -28,5 +28,19 @@ let matriz = [
   [4, 5, 6],
   [7, 8, 9],
 ];
-console.log(matriz);
-console.log(matriz[1][2]);
+// console.log(matriz);
+// console.log(matriz[1][2]);
+
+// Exemplo utilizando unshift()
+let myArray = [2, 3, 4];
+
+// Adicionando elementos no início do array
+myArray.unshift(1, 10, 20, 50);
+// console.log('My Array', myArray);
+
+
+// Exemplo utilizando shift()
+let mySecondArray = [2, 3, 4];
+let removedFirstElement = mySecondArray.shift();
+console.log(`My removedFirstElement ${removedFirstElement}`);
+console.log(`MySecondArray is ${mySecondArray}`);
